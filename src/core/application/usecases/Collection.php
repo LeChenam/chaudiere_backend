170541465c288bc71eb96b2f2dc724c1b9c387c9
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-06-05 12:03:46 +0200

    Create Collection.php

---
+++ src/core/application/usecases/Collection.php
@@ -0,0 +1,91 @@
+<?php
+namespace chaudiere\appli\core\application\usecases;
+
+use \Illuminate\Database\QueryException;
+use chaudiere\appli\core\application\exceptions\ExceptionDatabase;
+use chaudiere\core\domain\entities\Categorie;
+use chaudiere\core\domain\entities\Evenement;
+use chaudiere\appli\core\domain\exceptions\EntityNotFoundException;
+
+class Collection implements CollectionInterface
+{
+
+    /**
+     * @throws EntityNotFoundException
+     * @throws ExceptionDatabase
+     */
+    public function getCategories(): array{
+        try {
+            $categories = Categorie::all();
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new EntityNotFoundException("Table Categorie introuvable");
+        } catch (QueryException $e) {
+            throw new ExceptionDatabase("Erreur de requête : " . $e->getMessage());
+        }
+        return $categories->toArray();
+    }
+
+    /**
+     * @throws EntityNotFoundException
+     * @throws ExceptionDatabase
+     */
+    public function getCategorieById(int $id): array
+    {
+        try {
+            $categorie = Categorie::findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new EntityNotFoundException("Categorie $id introuvable");
+        } catch (QueryException $e) {
+            throw new ExceptionDatabase("Erreur de requête : " . $e->getMessage());
+        }
+        return $categorie->toArray();
+    }
+
+    /**
+     * @throws EntityNotFoundException
+     * @throws ExceptionDatabase
+     */
+    public function getEvenementById(string $id): array
+    {
+        try {
+            $prestation = Evenement::find($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new EntityNotFoundException("Prestation $id introuvable");
+        } catch (QueryException $e) {
+            throw new ExceptionDatabase("Erreur de requête : " . $e->getMessage());
+        }
+        return $prestation->toArray();
+    }
+
+    /**
+     * @throws EntityNotFoundException
+     * @throws ExceptionDatabase
+     */
+    public function getEvenementsByCategorie(int $categ_id): array
+    {
+        try {
+            $categorie = Categorie::findOrFail($categ_id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new EntityNotFoundException("Categorie $categ_id introuvable");
+        } catch (QueryException $e) {
+            throw new ExceptionDatabase("Erreur de requête : " . $e->getMessage());
+        }
+        return $categorie->evenements->toArray();
+    }
+
+    /**
+     * @throws EntityNotFoundException
+     * @throws ExceptionDatabase
+     */
+    public function getEvenements(): array
+    {
+        try {
+            $prestations = Evenement::all();
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new EntityNotFoundException("Table Prestation introuvable");
+        } catch (QueryException $e) {
+            throw new ExceptionDatabase("Erreur de requête : " . $e->getMessage());
+        }
+        return $prestations->toArray();
+    }
+}
