cb49cbd45550b8647c65fa42cb48e4a7a273dd45
Manech <chenamgg@gmail.com>
2025-06-05 11:24:23 +0200

    page accueil

--- src/conf/bootstrap.php
+++ src/conf/bootstrap.php
@@ -13,6 +13,13 @@ $app->addBodyParsingMiddleware();
 $app->addErrorMiddleware(true, false, false);
 
 $twig = \Slim\Views\Twig::create(__DIR__ . '/../webui/views', ['cache' => false, 'auto_reload' => true , 'strict_variables' => true]);
+$twig->getEnvironment()
+    ->addGlobal('globals', [
+            'css_dir'=> 'static/css',
+            'img_dir'=> 'static/img',
+        ]
+    );
+
 $app->add(\Slim\Views\TwigMiddleware::create($app, $twig));
 
 $app = (require_once __DIR__ . '/routes.php')($app);
--- src/conf/routes.php
+++ src/conf/routes.php
@@ -1,6 +1,13 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (Slim\App $app) {
+
+    $app->get('[/]', function (Request $request, Response $response) {
+        $view = \Slim\Views\Twig::fromRequest($request);
+        return $view->render($response, 'pages/ViewAccueil.twig');
+    })->setName('home');
 
     return $app;
 };
