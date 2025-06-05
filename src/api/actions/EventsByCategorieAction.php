d532257136deedf8be80cedcb5d7c52fd397dae5
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-06-05 16:00:58 +0200

    Create EventsByCategorieAction.php

---
+++ src/api/actions/EventsByCategorieAction.php
@@ -0,0 +1,43 @@
+<?php
+namespace chaudiere\api\actions;
+
+use chaudiere\core\application\usecases\Collection;
+use chaudiere\core\application\usecases\CollectionInterface;
+use chaudiere\core\domain\exceptions\EntityNotFoundException;
+use Slim\Exception\HttpNotFoundException;
+
+class EventsByCategorieAction {
+
+    private CollectionInterface $collection;
+    public function __construct() {
+        $this->collection = new Collection();
+    }
+
+    public function __invoke($request, $response, $args){
+        $id = $args['id'] ?? null;
+
+        if ($id == null) {
+            throw new \Slim\Exception\HttpBadRequestException($request, "Paramètre manquant");
+        } else{
+            //On tente de récupérer les catégories depuis le modèle
+            try {
+                $evenements = $this->collection->getEvenementsByCategorie($id);
+                $categorie = $this->collection->getCategorieById($id);
+            } catch (EntityNotFoundException $e) {
+                throw new HttpNotFoundException($request, $e->getMessage());
+            }
+
+            //Transformation des données
+            $data = [ 'type' => 'collection',
+                'categorie' => $categorie,
+                'count' => count($evenements),
+                'evenements' => $evenements ];
+            $response->getBody()->write(json_encode($data));
+        }
+
+        //renvoie des données
+        return
+            $response->withHeader('Content-Type','application/json')
+                ->withStatus(200);
+    }
+}
