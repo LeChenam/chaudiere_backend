6938a4ac8d0b3d4d975648ad7476e00f35c5231b
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-06-06 16:36:17 +0200

    Update CollectionInterface.php

--- src/core/application/usecases/CollectionInterface.php
+++ src/core/application/usecases/CollectionInterface.php
@@ -7,6 +7,6 @@ Interface CollectionInterface{
     public function getEvenementById(string $id): array;
     public function getEvenementsByCategorie(int $categ_id): array;
     public function getEvenements(): array;
-
     public function getEvenementsByPeriode(string $periode): array;
+    public function getEvenementsRanges(string $rangement): array;
 }
