d68b3e049dbfc4f964e5660b2eaffd4897c96cc7
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-06-10 11:54:54 +0200

    Update EventsByCategorieAction.php

--- src/api/actions/EventsByCategorieAction.php
+++ src/api/actions/EventsByCategorieAction.php
@@ -14,25 +14,68 @@ class EventsByCategorieAction {
     }
 
     public function __invoke($request, $response, $args){
+        $queryParams = $request->getQueryParams();
         $id = $args['id'] ?? null;
+        $periode = $queryParams['periode'] ?? null;
+        $rangement = $queryParams['sort'] ?? null;
 
         if ($id == null) {
             throw new \Slim\Exception\HttpBadRequestException($request, "Paramètre manquant");
         } else{
-            //On tente de récupérer les catégories depuis le modèle
-            try {
-                $evenements = $this->collection->getEvenementsByCategorie($id);
-                $categorie = $this->collection->getCategorieById($id);
-            } catch (EntityNotFoundException $e) {
-                throw new HttpNotFoundException($request, $e->getMessage());
-            }
+            if ($periode == null){
+                if ($rangement == null){
+                    //On tente de récupérer les catégories depuis le modèle
+                    try {
+                        $evenements = $this->collection->getEvenementsByCategorie($id);
+                        $categorie = $this->collection->getCategorieById($id);
+                    } catch (EntityNotFoundException $e) {
+                        throw new HttpNotFoundException($request, $e->getMessage());
+                    }
 
-            //Transformation des données
-            $data = [ 'type' => 'collection',
-                'categorie' => $categorie,
-                'count' => count($evenements),
-                'evenements' => $evenements ];
-            $response->getBody()->write(json_encode($data));
+                    //Transformation des données
+                    $data = [ 'type' => 'collection',
+                        'categorie' => $categorie,
+                        'count' => count($evenements),
+                        'evenements' => $evenements ];
+                    $response->getBody()->write(json_encode($data));
+                } else{
+                    //On tente de récupérer les catégories depuis le modèle
+                    try {
+                        $evenements = $this->collection->getSortedEventsByCategorie($id, $rangement);
+                        $categorie = $this->collection->getCategorieById($id);
+                    } catch (EntityNotFoundException $e) {
+                        throw new HttpNotFoundException($request, $e->getMessage());
+                    }
+
+                    //Transformation des données
+                    $data = [ 'type' => 'collection',
+                        'categorie' => $categorie,
+                        'count' => count($evenements),
+                        'evenements' => $evenements ];
+                    $response->getBody()->write(json_encode($data));
+                }
+            } else{
+                //On tente de récupérer les catégories depuis le modèle
+                try {
+                    $periodetab = explode(",",$periode);
+                    //On tente de récupérer les catégories depuis le modèle
+                    $evenements = [];
+                    for ($i = 0; $i < count($periodetab); $i++){
+                        $evenements = array_merge($evenements, $this->collection->getEventsByCategByPeriode($id, $periodetab[$i]));
+                    }
+                    $categorie = $this->collection->getCategorieById($id);
+                } catch (EntityNotFoundException $e) {
+                    throw new HttpNotFoundException($request, $e->getMessage());
+                }
+
+                //Transformation des données
+                $data = [ 'type' => 'collection',
+                    'categorie' => $categorie,
+                    'periode' => $periode,
+                    'count' => count($evenements),
+                    'evenements' => $evenements ];
+                $response->getBody()->write(json_encode($data));
+            }
         }
 
         //renvoie des données
