bac4a315e56b5b78c2c111653eadb27da1a6009e
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-06-11 11:06:35 +0200

    Ajoute le liens pour les catégories

--- src/api/providers/EventsLinksProvider.php
+++ src/api/providers/EventsLinksProvider.php
@@ -13,8 +13,9 @@ class EventsLinksProvider implements EventsLinksProviderInterface
         $routeParser = RouteContext::fromRequest($request)->getRouteParser();
 
         for ($i = 0; $i < count($events); $i++) {
-            $link = $routeParser->urlFor('api_events', ['id' => $events[$i]['id']]);
-            $events[$i]['links'] = ['self' => ['href' => $link]];
+            $linkEvent = $routeParser->urlFor('api_events', ['id' => $events[$i]['id']]);
+            $linkCategory = $routeParser->urlFor('api_categories', ['id' => $events[$i]['categorie_id']]);
+            $events[$i]['links'] = ['self' => ['href' => $linkEvent], 'categorie' => ['href' => $linkCategory]];
         }
 
         return $events;
